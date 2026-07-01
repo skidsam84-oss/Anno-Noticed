c7bd5490b321531ac79dbf7aad58c75d524aac24
skidsam84-oss <skidsam84@gmail.com>
2026-07-01 15:22:29 +0100

    Create __init__.py

---
+++ app/handlers/__init__.py
@@ -0,0 +1,3 @@
+from app.handlers.commands import router as commands_router
+from app.handlers.admin_handlers import router as admin_router
+from app.handlers.buyer_handlers import router as buyer_router
