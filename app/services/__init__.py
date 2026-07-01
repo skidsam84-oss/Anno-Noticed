5a5cc26bc3404eb94d1ed9eae968e7bb1923f96c
skidsam84-oss <skidsam84@gmail.com>
2026-07-01 16:49:08 +0100

    Create __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,4 @@
+"""Services package for Annopow Bot"""
+from app.services.ai_service import AIService
+from app.services.message_service import MessageService
+from app.services.analytics_service import AnalyticsService
